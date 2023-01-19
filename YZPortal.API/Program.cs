90f866a84ca8b5eda558767820086b5d50012d18
Chin Yi Zhe <cyz@annata.net>
2023-01-19 22:54:06 +0800

    Remove YZPortal.Worker and add nugets

--- YZPortal.API/Program.cs
+++ YZPortal.API/Program.cs
@@ -1,13 +1,32 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#region Adding services to container
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#endregion
+
 var app = builder.Build();
+
+#region WebApp scoped services
+
+// Scoped services
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    // EFCore
+
+    // DB seed and update
+}
+
+#endregion
+
+#region WebApp MiddleWare
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,5 +40,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+#endregion
 
 app.Run();
---
+++ YZPortal.WorkerSub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YZPortal.WorkerSub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
