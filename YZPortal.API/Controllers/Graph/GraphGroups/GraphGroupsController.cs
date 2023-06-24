602026ed1c94fe79bd0812db71808819d0a2c1a3
Chin Yi Zhe <cyz@annata.net>
2023-06-24 20:48:20 +0800

    Remove scope requirement on GetGraphGroupDisplayNamesForUser controller

--- YZPortal.API/Controllers/Graph/GraphGroups/GraphGroupsController.cs
+++ YZPortal.API/Controllers/Graph/GraphGroups/GraphGroupsController.cs
@@ -1,17 +1,23 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.Resource;
 using YZPortal.API.Controllers.ControllerTypes;
 using YZPortal.FullStackCore.Models.Abstracts;
 
 namespace YZPortal.API.Controllers.Graph.GraphGroups
 {
-    public class GraphGroupsController : ApiB2CController
+    public class GraphGroupsController : ApiBaseController
     {
+        private const string _ = "API.Access";
+
         public GraphGroupsController(IMediator mediator, LinkGenerator linkGenerator) : base(mediator, linkGenerator)
         {
         }
 
+        [Authorize(AuthenticationSchemes = Constants.AzureAdB2C)]
+        [RequiredScope(_)]
         [HttpGet]
         public async Task<ActionResult<SearchModel<Index.Model>>> GetGraphGroups([FromQuery] Index.Request request) => await _mediator.Send(request);
 
